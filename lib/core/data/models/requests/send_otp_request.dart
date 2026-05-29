61ee0fa6ffacf65f59c3024965e1b88f6a99e53c
Priyansh Vaghani <priyanshvaghani1604@gmail.com>
2026-05-29 17:06:02 +0530

    refactor: remove deprecated auth request classes and update SendOtpRequest and VerifyOtpRequest for improved JSON handling

--- lib/core/data/models/requests/send_otp_request.dart
+++ lib/core/data/models/requests/send_otp_request.dart
@@ -2,16 +2,17 @@ class SendOtpRequest {
   final String mobileNumber;
   final String countryCode;
 
-  SendOtpRequest({required this.mobileNumber, required this.countryCode});
+  const SendOtpRequest({required this.mobileNumber, required this.countryCode});
 
   factory SendOtpRequest.fromJson(Map<String, dynamic> json) {
     return SendOtpRequest(
-      mobileNumber: json['mobile_number'] ?? '',
-      countryCode: json['country_code'] ?? '',
+      mobileNumber: json['mobile_number'] as String? ?? '',
+      countryCode: json['country_code'] as String? ?? '',
     );
   }
 
-  Map<String, dynamic> toJson() {
-    return {'mobile_number': mobileNumber, 'country_code': countryCode};
-  }
+  Map<String, dynamic> toJson() => {
+    'mobile_number': mobileNumber,
+    'country_code': countryCode,
+  };
 }
--- lib/core/data/models/requests/verify_otp_request.dart
+++ lib/core/data/models/requests/verify_otp_request.dart
@@ -3,17 +3,23 @@ class VerifyOtpRequest {
   final String countryCode;
   final String otp;
 
-  VerifyOtpRequest({
+  const VerifyOtpRequest({
     required this.mobileNumber,
     required this.countryCode,
     required this.otp,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'mobile_number': mobileNumber,
-      'country_code': countryCode,
-      'otp': otp,
-    };
+  factory VerifyOtpRequest.fromJson(Map<String, dynamic> json) {
+    return VerifyOtpRequest(
+      mobileNumber: json['mobile_number'] as String? ?? '',
+      countryCode: json['country_code'] as String? ?? '',
+      otp: json['otp'] as String? ?? '',
+    );
   }
+
+  Map<String, dynamic> toJson() => {
+        'mobile_number': mobileNumber,
+        'country_code': countryCode,
+        'otp': otp,
+      };
 }
