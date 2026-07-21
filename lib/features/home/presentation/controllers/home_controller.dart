9ebba05944c7a35c7dc698309a24c7cf25b9b7f6
Priyansh Vaghani <priyanshvaghani1604@gmail.com>
2026-07-21 17:02:07 +0530

    Remove dev mock driver shortcuts from ride and home flows.

--- lib/features/home/presentation/controllers/home_controller.dart
+++ lib/features/home/presentation/controllers/home_controller.dart
@@ -163,7 +163,6 @@ class HomeController extends GetxController with WidgetsBindingObserver {
     analyticsService.logEvent('home_screen_viewed');
     _loadMapIcons();
     _initSequentialPermissions();
-    _addMockDrivers();
     _startActiveRidePolling();
     _loadHomeData().whenComplete(() async {
       // Product rule: call pending-review API only once when app session
@@ -1057,10 +1056,6 @@ class HomeController extends GetxController with WidgetsBindingObserver {
       return;
     }
     await toggleAddAddressBottomSheetForRecent(loc);
-  }
-
-  void _addMockDrivers() {
-    // Removed map driver markers
   }
 
   Future<void> _getCurrentLocation({
